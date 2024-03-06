2f72753aa3ff845fecf9ce0f754ba3680c3444fb
micdra01 <114472076+micdra01@users.noreply.github.com>
2024-03-06 14:07:11 +0100

    Something works

---
+++ infrastructure/Models/ComputerVisionReponseDto.cs
@@ -0,0 +1,68 @@
+﻿namespace infrastructure.Models;
+
+public class ComputerVisionReponseDto
+{
+    
+}
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+public class Line
+{
+    public string content { get; set; }
+    public List<double> boundingBox { get; set; }
+    public List<Span> spans { get; set; }
+}
+
+public class Metadata
+{
+    public int width { get; set; }
+    public int height { get; set; }
+}
+
+public class Page
+{
+    public double height { get; set; }
+    public double width { get; set; }
+    public double angle { get; set; }
+    public int pageNumber { get; set; }
+    public List<Word> words { get; set; }
+    public List<Span> spans { get; set; }
+    public List<Line> lines { get; set; }
+}
+
+public class ReadResult
+{
+    public string stringIndexType { get; set; }
+    public string content { get; set; }
+    public List<Page> pages { get; set; }
+    public List<object> styles { get; set; }
+    public string modelVersion { get; set; }
+}
+
+public class Root
+{
+    public ReadResult readResult { get; set; }
+    public string modelVersion { get; set; }
+    public Metadata metadata { get; set; }
+}
+
+public class Span
+{
+    public int offset { get; set; }
+    public int length { get; set; }
+}
+
+public class Span2
+{
+    public int offset { get; set; }
+    public int length { get; set; }
+}
+
+public class Word
+{
+    public string content { get; set; }
+    public List<double> boundingBox { get; set; }
+    public double confidence { get; set; }
+    public Span span { get; set; }
+}
+
---
+++ service/ComputerVisionService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Web;
+
+namespace service;
+
+public static class ComputerVisionService
+{
+    public static async void MakeRequest()
+    {
+        var client = new HttpClient();
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        // Request headers
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "b918311834b84b71aba458cf5d43830d");
+
+        // Request parameters
+        queryString["features"] = "read";
+        queryString["model-name"] = "";
+        queryString["language"] = "en";
+        queryString["smartcrops-aspect-ratios"] = "";
+        queryString["gender-neutral-caption"] = "False";
+        var uri = "https://cvfoodallergenes.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&" + queryString;
+
+        HttpResponseMessage response;
+
+        // Create your object
+        var requestBody = new
+        {
+            url = "https://assets.summerbird.dk/media/wdof5euo/5509-marcipan-200-g-vegansk.jpg"
+        };
+
+        // Serialize the object to JSON
+        string jsonBody = JsonSerializer.Serialize(requestBody);
+
+        // Request body
+        using (var content = new StringContent(jsonBody, Encoding.UTF8, "application/json"))
+        {
+            response = await client.PostAsync(uri, content);
+        }
+
+        // Handle response
+        if (response.IsSuccessStatusCode)
+        {
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Response: " + responseContent);
+        }
+        else
+        {
+            Console.WriteLine("Error: " + response.StatusCode);
+        }
+    }
+}
