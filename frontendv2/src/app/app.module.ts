0a2a6f0e30d9c9cdd25bf397a32c8bb4c0363ef7
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2024-03-15 09:50:29 +0100

    JWT fixed

--- frontendv2/src/app/app.module.ts
+++ frontendv2/src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthHttpInterceptor} from "../interceptors/auth-http-interceptor";
+import {TokenService} from "../services/TokenService";
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+    TokenService,],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
